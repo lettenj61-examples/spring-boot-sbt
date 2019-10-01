71fac43b8ff7d4022ea00bd5e36fdd90f1d71daa
lettenj61 <lettenj61@users.noreply.github.com>
2019-10-02 08:22:04 +0900

    change Jackson configuration to `@Component`

---
+++ src/main/scala/com/github/lettenj61/springboot/Configurations.scala
@@ -0,0 +1,13 @@
+package com.github.lettenj61.springboot
+
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import org.springframework.stereotype.Component
+
+@Component
+class ObjectMapperConfig extends Jackson2ObjectMapperBuilderCustomizer {
+  override def customize(builder: Jackson2ObjectMapperBuilder): Unit = {
+    val _ = builder.modules(DefaultScalaModule)
+  }
+}
